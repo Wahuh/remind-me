c93e9d592ef9e0444005000f2fc8dddcc14612a5
Thanh Doan <tmdoan98@gmail.com>
2019-01-26 12:55:21 +0000

    remove service worker

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 
-if ("serviceWorker" in navigator && "PushManager" in window) {
-    navigator.serviceWorker.register("./serviceWorker.js")
-    .then(reg => console.log("Service worker registered", reg.scope))
-    .catch(error => console.log("Error, service worker not registered", error));
-}
+// if ("serviceWorker" in navigator && "PushManager" in window) {
+//     navigator.serviceWorker.register("./serviceWorker.js")
+//     .then(reg => console.log("Service worker registered", reg.scope))
+//     .catch(error => console.log("Error, service worker not registered", error));
+// }
 
 ReactDOM.render(<App />, document.getElementById("root"));
