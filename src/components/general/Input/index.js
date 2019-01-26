2aa75a082656aa4c0b6e711ae60dde7c911144e3
Thanh Doan <tmdoan98@gmail.com>
2019-01-26 12:17:44 +0000

    handle enter key

--- src/components/general/Input/index.js
+++ src/components/general/Input/index.js
@@ -1,33 +1,75 @@
-import React from "react";
-import styles from "./Input.css";
-import { bool, func, string } from "prop-types";
 
-const Input = (props) => {
-    let className = styles.Input;
-    if (props.className) {
-        className += ` ${props.className}`;
+import React, { Fragment } from "react";
+import styles from "./Input.css";
+
+const Input = ({
+    autoFocus, 
+    className, 
+    contentEditable, 
+    floatLabel, 
+    label, 
+    max, 
+    min, 
+    name, 
+    onEnter, 
+    onChange, 
+    placeholder, 
+    required, 
+    type, 
+    value, 
+    validation, 
+    successMessage, 
+    onKeyPress,
+    onBlur
+}) => {
+    function onEnterPress(event) {
+        if (event.defaultPrevented) {
+            return; // Should do nothing if the default action has been cancelled
+        }
+        let handled = false;
+        if (event.key !== undefined) {
+            if (event.key === "Enter") {
+                onEnter();
+                handled = true;
+            } else {
+                onKeyPress && onKeyPress();
+            }
+        } else if (event.keyCode !== undefined) {
+            if (event.keyCode == 13) {
+                onEnter();
+                handled = true;
+            } else {
+                onKeyPress && onKeyPress();
+            }
+        }
+
+        if (handled) {
+            event.preventDefault();
+        }
     }
 
     return (
-        <input 
-        className={className} 
-        type={props.type} 
-        autoFocus={props.autoFocus} 
-        placeholder={props.placeholder}
-        onChange={props.onChange}
-        value={props.value}
-        max={props.max}
-        min={props.min}
-        />
-    );
-}
+        <Fragment>
 
-Input.propTypes = {
-    autoFocus: bool,
-    className: string,
-    onClick: func.isRequired,
-    type: string,
-    placeholder: string,
+            <input 
+            className={className ? `${styles.Input} ` + className : styles.Input} 
+            type={type} 
+            contentEditable={contentEditable}
+            autoFocus={autoFocus} 
+            // ref={focus && (input => input && input.focus())}
+            placeholder={placeholder}
+            value={value}
+            max={max}
+            min={min}
+            id={name}
+            name={name}
+            onBlur={onBlur}
+            onChange={onChange}
+            onKeyPress={onEnterPress}
+            data-success-message={successMessage}
+            />
+        </Fragment>
+    );
 }
 
 export default Input;
