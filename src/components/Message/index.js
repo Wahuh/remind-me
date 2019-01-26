d48aa6c6a0f0e967206e188b1f349cf0626b10b7
Thanh Doan <tmdoan98@gmail.com>
2019-01-26 14:36:29 +0000

    refactor component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import Input from "../general/Input";
-import Typography from "../general/Typography";
 import styles from "./Message.css";
-import * as constants from "../../constants";
 
-const Message = (props) => {
+const Message = ({ value, onEnter, onChange }) => {
     return (
         <div className={styles.Message}>
-            <div className={props.error ? styles.ErrorMessageShow : styles.ErrorMessageHide}>
-                {props.type === constants.MESSAGE_ERROR && <Typography type="body2">Please enter a message</Typography>}
-                {props.type === constants.TIME_ERROR && <Typography type="body2">Please enter a time</Typography>}
-            </div>
-            <Input value={props.value} onEnter={props.onEnter} onChange={props.onChange} className={styles.MessageInput} placeholder="Enter a message" />
+            <Input value={value} onEnter={onEnter} onChange={onChange} className={styles.MessageInput} placeholder="Enter a message" />
         </div>
     );
 }
