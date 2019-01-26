1e9d7934436e2fdd35c8657e66be25280945d354
Thanh Doan <tmdoan98@gmail.com>
2019-01-26 12:12:21 +0000

    change notification support detection

--- src/App.js
+++ src/App.js
@@ -3,35 +3,79 @@ import AddReminder from "./components/AddReminder";
 import ReminderList from "./components/ReminderList";
 import RemindMe from "./components/RemindMe";
 import styles from "./App.css";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            reminders: [],
-        };
-        this.addReminder = this.addReminder.bind(this);
+    state = {
+        reminders: [],
+        hasNotifications: false,
+        hasPermissions: false
+    };
+
+    componentDidMount() {
+        this.detectNotificationSupport();
     }
 
-    addReminder(reminder) {
-        this.setState({
-            reminders: [...this.state.reminders, reminder]
-        })
+    detectNotificationSupport = () => {
+        if ("Notification" in window) {
+            if (Notification.permission === "granted") {
+                this.setState(
+                    prevState => ({ 
+                        ...prevState, 
+                        hasNotifications: true,
+                        hasPermissions: true
+                    })
+                );
+            }
+            else if (Notification.permission === "denied" || Notification.permission === "default") {
+                this.setState(
+                    prevState => ({
+                        ...prevState, 
+                        hasNotifications: true,
+                        hasPermissions: false
+                    })
+                );
+            }
+        }
+    }
+
+    requestNotificationPermissions = () => {
+        console.log(Notification.permission);
+        Notification.requestPermission()
+        .then(permission => {
+            console.log("permission", permission);
+            if (permission === "granted") {
+                this.setState(prevState => ({ ...prevState, hasPermissions: true }));
+            }
+        });
+    }
+
+    addReminder = reminder => {
+        this.setState(prevState => ({
+            ...prevState,
+            reminders: [...prevState.reminders, reminder]
+        }))
     }
 
     render() {
+        const { hasNotifications, hasPermissions } = this.state;
         return (
             <div className={styles.App}>
-                <div className={styles.Container}>
-                    <RemindMe />
-                </div>
+                <NavBar 
+                    hasNotifications={hasNotifications} 
+                    hasPermissions={hasPermissions}
+                    onRequest={this.requestNotificationPermissions}
+                />
 
-                <div className={styles.Container}>
-                    <AddReminder onAdd={this.addReminder} />
-                    <div className={styles.ReminderListContainer}>
+                <main>
+                    <div className={styles.Container}>
+                        <AddReminder onAdd={this.addReminder} />
+                    </div>
+
+                    <div className={styles.Container}>
                         <ReminderList reminders={this.state.reminders} />
                     </div>
-                </div>
+                </main>
             </div>
         );
     }
